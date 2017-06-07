edbc8a281c38b73a896a60dba9d30577fd2edb26
Stefan Henneken <stefan.henneken@live.com>
2017-06-07 17:48:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Sample06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Run();
+        }
+        public void Run()
+        {
+            Console.WriteLine("Start Run");
+            const int iAmount = 100000;
+
+            // sequenziell
+            int[] iValuesSeq = new int[iAmount];
+            iValuesSeq[0] = 1;
+            for (int i = 1; i < iAmount; i++)
+            {
+                iValuesSeq[i] = iValuesSeq[i - 1] + 1;
+            }
+
+            // parallel
+            int[] iValuesPar = new int[iAmount];
+            iValuesPar[0] = 1;
+            Parallel.For(1, iAmount, i =>
+                {
+                    iValuesPar[i] = iValuesPar[i - 1] + 1;
+                });
+
+            // Test
+            long lSumSeq = 0;
+            long lSumPar = 0;
+            for (int i = 0; i < iAmount; i++)
+            {
+                lSumSeq += iValuesSeq[i];
+                lSumPar += iValuesPar[i];
+            }
+            Console.WriteLine("Seq: {0}  Par: {1}", lSumSeq, lSumPar);
+
+            Console.WriteLine("End Run");
+            Console.ReadLine();
+        }
+
+    }
+}
